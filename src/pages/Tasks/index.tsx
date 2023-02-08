dba4b96661ca0e43e63644281c0a8fd00700c798
leticea <nascimento.leticeah@gmail.com>
2023-02-08 11:34:52 -0300

    editando página tasks

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,3 +1,38 @@
+import { PlusCircle } from "phosphor-react";
+import {
+  CreateTaskButton,
+  ListContainer,
+  HomeContainer,
+  InfoContainer,
+  SearchContainer,
+  TaskContainer,
+  TaskInput,
+} from "./styles";
+
 export function Tasks() {
-  return <h1>Tasks</h1>
+  return (
+    <HomeContainer>
+      <SearchContainer>
+        <TaskInput type="text" placeholder="Adicione uma tarefa" />
+
+        <CreateTaskButton type="submit">
+          Criar <PlusCircle size={20} />
+        </CreateTaskButton>
+      </SearchContainer>
+
+      <TaskContainer>
+        <InfoContainer>
+          <div className="createdTasks">Tarefas criadas</div>
+          <div className="createdTasksCounter">0</div>
+
+          <div className="completedTasks">Concluídas</div>
+          <div className="completedTasksCounter">0</div>
+        </InfoContainer>
+
+        <ListContainer>
+
+        </ListContainer>
+      </TaskContainer>
+    </HomeContainer>
+  );
 }
