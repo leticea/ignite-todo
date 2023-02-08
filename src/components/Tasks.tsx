3e1c8b67d7df6e18b846ca432c303e992aefdc04
leticea <nascimento.leticeah@gmail.com>
2023-02-08 20:18:09 -0300

    editando o estilo do checkbox

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -5,12 +5,12 @@ import styles from "./Tasks.module.css";
 export function Tasks() {
   return (
     <div className={styles.taskContainer}>
-        <input className={styles.taskCheckbox} type="checkbox" />
-        <p>
-          Integer urna interdum massa libero auctor neque turpis turpis semper.
-          Duis vel sed fames integer.
-        </p>
-        <Trash size={24} color="#808080" />
+      <input className={styles.taskCheckbox} type="checkbox" />
+      <p>
+        Integer urna interdum massa libero auctor neque turpis turpis semper.
+        Duis vel sed fames integer.
+      </p>
+      <Trash size={24} color="#808080" />
     </div>
   );
 }
