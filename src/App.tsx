124328c4382caf277905be4d1b85292bbb971c97
leticea <nascimento.leticeah@gmail.com>
2023-02-09 10:00:37 -0300

    criando as tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
+
 import "./global.css";
 
+const tasks = [
+  {
+    id: 1,
+    description: "Molhar as plantas",
+    done: true
+  },
+  {
+    id: 2,
+    description: "Ir ao mercado",
+    done: false
+  },
+  {
+    id: 1,
+    description: "Fazer exercício",
+    done: false
+  }
+]
 
 export function App() {
   return (
